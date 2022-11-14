fadbdac80f0c031e2d83e67cc767e193ecdfee01
xframee <alexander@kmax.dk>
2022-11-14 15:25:09 +0100

    setting up ef

--- assignment2backend/NorthwindContext.cs
+++ assignment2backend/NorthwindContext.cs
@@ -5,11 +5,15 @@ namespace EF
 {
     public class NorthwindContext : DbContext
     {
+        const string connectionString = "host=localhost;db=imdb;uid=postgres;pwd=paranormalA1";
+
+        public DbSet<ShowInfo> ShowInfos { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
 
-            optionsBuilder.UseNpgsql("host=localhost;db=imdb;uid=postgres;pwd=paranormalA1");
+            optionsBuilder.UseNpgsql(connectionString);
         }
     }
 }
--- assignment2backend/Program.cs
+++ assignment2backend/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using EF;
+
+using var db = new NorthwindContext();
+
+foreach (var info in db.ShowInfos)
+{
+    Console.WriteLine(info.TConst);
+}
