78882583ae1ee12c97ac777de6e484dc9a905aa6
xframee <alexander@kmax.dk>
2022-11-14 15:12:18 +0100

    removed hello world

---
+++ assignment2backend/NorthwindContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseNpgsql("host=localhost;db=imdb;uid=postgres;pwd=paranormalA1");
+        }
+    }
+}
+
---
+++ assignment2backend/ShowInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EF
+{
+    public class ShowInfo
+    {
+        public string TConst { get; set; }
+        public string Type { get; set; }
+        public string PrimaryTitle { get; set; }
+        public string OriginalTitle { get; set; }
+        public bool IsAduldt { get; set; }
+        public string StartYear { get; set; }
+        public string EndYear { get; set; }
+        public int RunTime { get; set; }
+    }
+}
