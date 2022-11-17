1407c5c8fb45e3f1145434d1eedfbd2ed08cbc8d
Alissa Hansen <79077747+AlissaHansen@users.noreply.github.com>
2022-11-17 15:22:26 +0100

    Added first controller

---
+++ WebServer/Controllers/ShowInfosController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace WebServer.Controllers
+{
+    [Route("api/showInfos")]
+    public class ShowInfosController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from controller";
+        }
+    }
+}
+
